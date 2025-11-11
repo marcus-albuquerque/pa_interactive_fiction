ef14099b2d38edf5ab0d334dabe2370dbb873515
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-11-10 22:43:23 -0300

    Alterações de tela e desenvolvimento 10/11/25

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762742660|14054006';
+const CACHE_VERSION = '1762825203|8626375108';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas_FI-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
