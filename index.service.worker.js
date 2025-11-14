15430ddf18bcdfd2ddf3db9700f6af38e2b6e13a
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-11-13 22:39:09 -0300

    Textos das salas alterados e melhorados, nova layout de tela e apresentação 13/11/25

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762825203|8626375108';
+const CACHE_VERSION = '1763079398|213442384';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas_FI-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
